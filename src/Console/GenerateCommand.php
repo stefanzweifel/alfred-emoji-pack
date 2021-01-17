72ee498d9b0c0be2490894e87127de60ac7fad1f
Stefan Zweifel <hello@stefanzweifel.io>
2021-01-17 20:15:22 +0100

    WIP

--- src/Console/GenerateCommand.php
+++ src/Console/GenerateCommand.php
@@ -11,33 +11,11 @@ use SplFileInfo;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
-use Wnx\Emoji\Emoji;
-use Wnx\Emoji\Parser;
 use ZipArchive;
 
 class GenerateCommand extends Command
 {
     protected const PATH_TO_DIST_DIRECTORY = __DIR__ . '/../../dist/';
-
-    /** @var string */
-    protected const EMOJI_VERSION = '13.1';
-
-    /** @var int */
-    protected $now;
-
-    /** @var string */
-    protected $deprecationNotice = '# deprecations'.PHP_EOL;
-
-    /** @var \Spatie\Emoji\Generator\Emoji[] */
-    protected $emojis;
-
-    /** @var array[] */
-    protected $emojisArray;
-
-    /** @var array[] */
-    protected $groups;
 
     /** @var array[] */
     protected $emojiToNames;
@@ -46,13 +24,13 @@ class GenerateCommand extends Command
     {
         $this
             ->setName('generate')
-            ->setDescription('Generate the package code from the emoji docs');
+            ->setDescription('Generate Alfred Snippet Pack with Emojis.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->clearDistDirectory();
-        
+
         $this->createSnippets();
 
         $this->createAlfredSnippetsArchive();
@@ -70,8 +48,8 @@ class GenerateCommand extends Command
                 'snippet' => $emojiCharacter,
                 'uuid' => $uuid->toString(),
                 'name' => "{$emojiCharacter} {$names}" . ($tags) ?: "- {$tags}",
-                'keyword' => ":{$description}:"
-            ]
+                'keyword' => ":{$description}:",
+            ],
         ];
     }
 
@@ -107,23 +85,18 @@ class GenerateCommand extends Command
         $zipArchive = new ZipArchive();
         $zipArchive->open('Emoji Pack Neo.alfredsnippets', ZipArchive::CREATE | ZipArchive::OVERWRITE);
 
-        // Create recursive directory iterator
         /** @var SplFileInfo[] $files */
         $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($rootPath), RecursiveIteratorIterator::LEAVES_ONLY);
 
         foreach ($files as $name => $file) {
-            // Skip directories (they would be added automatically)
             if (! $file->isDir()) {
-                // Get real and relative path for current file
                 $filePath = $file->getRealPath();
                 $relativePath = substr($filePath, strlen($rootPath) + 1);
 
-                // Add current file to archive
                 $zipArchive->addFile($filePath, $relativePath);
             }
         }
 
-        // Zip archive will be created only after closing object
         $zipArchive->close();
     }
 
@@ -132,14 +105,12 @@ class GenerateCommand extends Command
         $encodedEmojiToNames = file_get_contents(__DIR__ . '/../../node_modules/gemoji/emoji-to-name.json');
         $this->emojiToNames = json_decode($encodedEmojiToNames, true);
 
-
         $encodedEmojis = file_get_contents(__DIR__ . '/../../node_modules/gemoji/index.json');
         $emojis = json_decode($encodedEmojis, true);
 
 
         // Create Snippets
         foreach ($emojis as $emoji) {
-
             $uuid = Uuid::uuid4();
 
             $snippet = $this->generateSnippet($emoji, $uuid);
